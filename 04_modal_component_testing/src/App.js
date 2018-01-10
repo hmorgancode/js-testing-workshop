173d65168653514734761019c3bc6b801f284bac
Henry Morgan <hmorgan@patientslikeme.com>
2018-01-10 14:10:09 -0500

    add modal testing for snapshots, shallow, and enzyme mock user input

--- 04_modal_component_testing/src/App.js
+++ 04_modal_component_testing/src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ModalWithoutState as Modal } from './Modal';
 
 class App extends Component {
+
+  state = {
+    showModal: false,
+  }
+
+  renderModal = () => {
+    if (!this.state.showModal) {
+      return null;
+    }
+
+    return (
+      <Modal
+        title="hello yes i am a modal"
+        foot={<p>this is a modal footer</p>}
+        handleCloseModal={() => this.setState({ showModal: false })}
+      >
+        <p>and here's some modal text</p>
+      </Modal>
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="is-dark">
+        <div id="app" className="container">
+          <section id="header" className="hero is-info">
+            <div className="hero-body container has-text-centered">
+              <h1 className="title">
+
+              </h1>
+              <h2 className="subtitle">
+
+              </h2>
+            </div>
+          </section>
+
+          <section className="section">
+            <div className="control">
+              <button className="button is-primary" onClick={() => this.setState({ showModal: true })}>Open Modal</button>
+            </div>
+            { this.renderModal() }
+          </section>
+
+
+          <footer className="footer">
+              <div className="content has-text-centered">
+
+              </div>
+          </footer>
+        </div>
       </div>
     );
   }
---
+++ 04_modal_component_testing/src/Modal.test.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ModalWithoutState as Modal } from './Modal';
+// import renderer from 'react-test-renderer';
+
+function ModalBody() {
+  return <p>Modal Body</p>;
+}
+
+// it('renders correctly with react-test-renderer', () => {
+//   const tree = renderer
+//     .create(<Modal><ModalBody /></Modal>)
+//     .toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+// test('renders correctly with enzyme', () => {
+//   // Snapshot testing!
+//   expect(shallow(<Modal><ModalBody /></Modal>)).toMatchSnapshot();
+// });
+describe('Modal', () => {
+  it('closes when the close button is clicked', () => {
+    const mockClose = jest.fn();
+    const modal = shallow(<Modal handleCloseModal={mockClose} />);
+    modal.find('.js-close-modal').simulate('click');
+    expect(mockClose).toHaveBeenCalled();
+  });
+
+  it('closes when the area outside of the modal is clicked', () => {
+    const mockClose = jest.fn();
+    const modal = shallow(<Modal handleCloseModal={mockClose} />);
+    modal.find('.js-modal-background').simulate('click');
+    // again, this is basically what toHaveBeenCalled is doing behind the scenes:
+    expect(mockClose.mock.calls.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('renders the provided modal title', () => {
+    const modal = shallow(<Modal title="Test Title" />);
+    expect(modal.find('.js-modal-title').text()).toBe('Test Title');
+  });
+
+  it('renders its children as the modal body', () => {
+    const modal = shallow(<Modal><ModalBody /></Modal>);
+    expect(modal.find(ModalBody).length).toBe(1);
+  });
+
+  it('renders the provided modal footer', () => {
+    const modal = shallow(<Modal foot={<div className="js-test-foot"></div>} />);
+    expect(modal.find('.js-test-foot').length).toBe(1);
+  });
+});
